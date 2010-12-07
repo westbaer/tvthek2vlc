fd4f7a222bf51a7451035c35265470ed167669b3
Nicolas Haunold <westbaer@gmail.com>
2010-12-07 16:37:16 +0100

    initial commit

---
+++ tvthek2vlc.js
@@ -0,0 +1,29 @@
+// copyright 2010 nicolas haunold <nicolas@haunold.me>
+
+function ___tvthek2vlcize___() {
+    var playlist_button = document.getElementById('open_playlist');
+    if(playlist_button != 'undefined' && playlist_button != null) {
+	var playlist_url = playlist_button.href;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", playlist_url, true);
+	xhr.onreadystatechange = function() {
+	    if(xhr.readyState == 4) {
+		var doc = xhr.responseXML;
+		if(doc != 'undefined' && doc != null) {
+		    var new_url = doc.getElementsByTagName('ref')[0];
+		    if(new_url != 'undefined' && new_url != null) {
+			new_url = new_url.getAttribute('href');
+			if(new_url != '' && new_url != 'undefined' && new_url != null) {
+			    playlist_button.href=new_url;
+			}
+		    }
+		}
+	    }
+	};
+
+	xhr.send();
+    }
+}
+
+___tvthek2vlcize___();
