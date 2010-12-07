3135d349193d8f379c7a82d31f9063910e8c828b
Nicolas Haunold <westbaer@gmail.com>
2010-12-07 17:13:15 +0100

    some videos aren't returning the text/xml content-type. *sigh*

--- tvthek2vlc.js
+++ tvthek2vlc.js
@@ -2,15 +2,17 @@
 
 function ___tvthek2vlcize___() {
     var playlist_button = document.getElementById('open_playlist');
-    if(playlist_button != 'undefined' && playlist_button != null) {
+    if(playlist_button != null) {
 	var playlist_url = playlist_button.href;
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", playlist_url, true);
 	xhr.onreadystatechange = function() {
 	    if(xhr.readyState == 4) {
-		var doc = xhr.responseXML;
-		if(doc != 'undefined' && doc != null) {
+		var parser = new DOMParser();
+		doc = parser.parseFromString(xhr.responseText, "text/xml");
+
+		if(doc != null) {
 		    var new_url = doc.getElementsByTagName('ref')[0];
 		    if(new_url != 'undefined' && new_url != null) {
 			new_url = new_url.getAttribute('href');
